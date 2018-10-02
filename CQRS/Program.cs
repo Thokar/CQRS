3c20ea0e5465ba5edbe19de5591519322fd4538f
GARBSEN1\karbe <Thomas.karbe@Arvato.com>
2018-10-02 14:00:12 +0200

    .

--- CQRS/Program.cs
+++ CQRS/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using CQRS.CQRS;
+using System;
 
 namespace CQRS
 {
-  class Program
+  public class Program
   {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      CQRSLogic();
+
+      Console.ReadLine();
+    }
+
+    public static void ClassicLogic()
+    {
+      var p = new Classic.Person(); // Working with object reference
+      p.Age = 23; // No audits, no tracking, no roleback 
+      Console.WriteLine("Age: " + p.Age);
+    }
+
+    public static void CQRSLogic()
+    {
+      var eb = new EventBroker();
+      var p = new CQRS.Person(eb);
+      int age;
+
+      eb.Command(new ChangeAgeCommand(p, 123));
+
+      foreach (var e in eb.AllEvents)
+      {
+        Console.WriteLine(e);
+      }
+
+      age = eb.Query<int>(new AgeQuery { Target = p });
+      Console.WriteLine("Age: " + age);
+
+      Console.WriteLine("Reverting");
+      eb.UndoLast(); 
+
+      foreach (var e in eb.AllEvents)
+      {
+        Console.WriteLine(e);
+      }
+
+      age = eb.Query<int>(new AgeQuery { Target = p });
+      Console.WriteLine("Age: " + age);
     }
   }
 }
